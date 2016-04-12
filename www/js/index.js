2654e35ba454e58b8acda7a2807f9dc62c99ea30
ipavl <ipavl@users.noreply.github.com>
2016-04-11 23:17:54 -0400

    Add ability to play individual songs and display their ID3 tags
    ID3 tags uses jsmediatags: https://github.com/aadsm/jsmediatags

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,5 @@
+var jsmediatags = window.jsmediatags;
+
 var app = {
     // Application Constructor
     initialize: function () {
@@ -14,7 +16,27 @@ var app = {
 
     // deviceready Event Handler
     onDeviceReady: function () {
-        // TODO
+        // Listener to play a file when it is selected
+        file_select.onchange = function() {
+            var file = this.files[0];
+            player.src = URL.createObjectURL(file);
+            player.play();
+
+            jsmediatags.read(file, {
+                onSuccess: function(jsonTags) {
+                    var tags = jsonTags.tags;
+
+                    // Update the Now Playing information
+                    np_title.innerText = tags.title;
+                    np_artist.innerText = tags.artist;
+                    np_album.innerText = tags.album;
+                    np_year.innerText = tags.year;
+                },
+                onError: function(error) {
+                    alert(error);
+                }
+            });
+        }
     },
 };
 
